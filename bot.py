f9fe3e068052cfdeed7cb61f8511a0ee77c48388
abrordeveloper <99119980+abrordevoloper@users.noreply.github.com>
2026-04-15 09:41:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 # =====================================================
 # ADMIN ID — @userinfobot ga /start yuboring, ID oling
 # =====================================================
-ADMIN_IDS = [123456789]  # <- O'z ID'ingizni yozing
+ADMIN_IDS = [1986282464]  # <- O'z ID'ingizni yozing
 
 DB_PATH = "questions.db"
 
